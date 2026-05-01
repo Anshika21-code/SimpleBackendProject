1c126269db273b7f071e5b367f86b2e0a2ebc946
Anshika Tank <anshikatank2110@gmail.com>
2026-05-01 12:55:37 +0530

    basic server setup done

---
+++ controllers/authController.js
@@ -0,0 +1,63 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils/generateToken.js";
+
+// ✅ REGISTER
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // check user
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // save user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "Registered successfully",
+    });
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// ✅ LOGIN
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // find user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email" });
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Wrong password" });
+    }
+
+    // generate token
+    const token = generateToken(user);
+
+    res.json({
+      message: "Login success",
+      token,
+    });
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
---
+++ models/Task.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  userId: mongoose.Schema.Types.ObjectId
+});
+
+export default mongoose.model("Task", taskSchema);
