332f12458b7bd1b6e1e066d911130eb203101844
Anshika Tank <anshikatank2110@gmail.com>
2026-05-01 14:35:38 +0530

    auth token and verification done

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,11 +1,20 @@
 import jwt from "jsonwebtoken";
 
 export const protect = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
 
-  if (!token) return res.status(401).json({ message: "No token" });
+    if (!token) {
+      return res.status(401).json({ message: "No token, access denied" });
+    }
 
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  req.user = decoded;
-  next();
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded; // { id, role }
+
+    next();
+
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
 };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,12 @@
 // routes/authRoutes.js
 import express from "express";
 import { register, login } from "../controllers/authController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/", protect, getTasks);
 
 export default router;
